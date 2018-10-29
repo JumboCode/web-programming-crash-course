d713c226231e09477233d764ff463a0124e0959b
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-10-28 21:02:32 -0400

    Revert "posting"
    This reverts commit b7be8fc3aaa76fb48a5e5626ed1e424508fe210a.

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
-posts = []
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        req_data = request.get_json()
-        message = req_data['message']
-        if (message):
-            posts.append(message)
-            return ("Posted message: " + message)
-        else:
-            return "Error: Did not recieve 'message' in POST request"
-    else:
-        response = "Posts: \n"
-        for post in posts:
-            response = response + post + "\n"
-        return response
+@app.route('/')
+def hello():
+    return 'Hello, World!'
